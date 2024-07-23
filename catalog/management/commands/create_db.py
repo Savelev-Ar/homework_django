46cddef94eb0be12c0a5eeff4b634e48006b6d23
Arkady V. Savelev <saversavic@gmail.com>
2024-07-23 15:22:06 +0500

    changes to comply with PEP-8

--- catalog/management/commands/create_db.py
+++ catalog/management/commands/create_db.py
@@ -2,6 +2,7 @@ from django.core.management import BaseCommand
 from configparser import ConfigParser
 import psycopg2
 
+
 def read_config_db(filename="database.ini", section="postgresql"):
     # create a parser
     parser = ConfigParser()
@@ -16,20 +17,22 @@ def read_config_db(filename="database.ini", section="postgresql"):
         raise Exception(
             'Section {0} is not found in the {1} file.'.format(section, filename))
     return db
+
+
 class Command(BaseCommand):
     """
     команда для создания БД заново
     """
     def handle(self, *args, **options):
-        self.connect_params = read_config_db()
-        self.database_name = 'catalog'
+        connect_params = read_config_db()
+        database_name = 'catalog'
 
-        conn = psycopg2.connect(dbname='postgres', **self.connect_params)
+        conn = psycopg2.connect(dbname='postgres', **connect_params)
         conn.autocommit = True
         cur = conn.cursor()
 
-        cur.execute(f"DROP DATABASE IF EXISTS {self.database_name}")
-        cur.execute(f"CREATE DATABASE {self.database_name}")
+        cur.execute(f"DROP DATABASE IF EXISTS {database_name}")
+        cur.execute(f"CREATE DATABASE {database_name}")
 
         conn.commit()
         conn.close()
