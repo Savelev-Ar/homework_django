b1ae4b60ebe45e0f3ca3118269f68b9041fd11be
Arkady V. Savelev <saversavic@gmail.com>
2024-07-29 19:22:51 +0500

    A new controller and template have been created to display a single product, Created for a template with the main menu

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from catalog.views import contacts, products
+from catalog.views import contacts, product, catalog
 
 app_name = 'students_list'
 
 urlpatterns = [
     path('contacts/', contacts, name='contacts'),
-    path('', products, name='products')
+    path('', product, name='product'),
+    path('catalog/', catalog, name='catalog')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -7,9 +7,17 @@ def contacts(request):
     return render(request, 'contacts.html')
 
 
-def products(request):
+def catalog(request):
     products_list = Product.objects.all()
     context = {
         'object_list': products_list
-   }
+    }
     return render(request, 'products.html', context)
+
+
+def product(request):
+    product = Product.objects.get(name='коробокс')
+    context = {
+        'object': product
+    }
+    return render(request, 'product.html', context)
