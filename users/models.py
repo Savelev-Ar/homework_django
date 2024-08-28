bd216583b6920a77f9f68b314e1b31a4e57d23ec
Arkady V. Savelev <saversavic@gmail.com>
2024-08-28 20:41:02 +0500

    create Users aplication, create_su command

---
+++ users/models.py
@@ -0,0 +1,34 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(
+        unique=True,
+        verbose_name='адрес почты',
+        help_text='введите адрес электрической почты'
+    )
+    phone = models.CharField(
+        max_length=35,
+        verbose_name='телефон',
+        help_text='Введите номер телефона',
+        blank=True,
+        null=True
+    )
+    avatar = models.ImageField(
+        upload_to='users/',
+        verbose_name='Аватар',
+        help_text='Загрузитэ рисунок аватара',
+        blank=True,
+        null=True
+    )
+    country = models.CharField(
+        max_length=35,
+        verbose_name='телефон',
+        help_text='Введите номер телефона',
+        blank=True,
+        null=True
+    )
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
